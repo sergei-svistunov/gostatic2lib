39cc2036da9e1dc5c0daadc5a3aeab83345eef2f
Sergey Svistunov <svistunov.sergey@lazada.com>
2015-08-13 17:08:10 +0300

    Removed base64

---
+++ gobyte_writer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type GoByteWriter struct {
+	w io.Writer
+}
+
+func NewGoByteWriter(w io.Writer) *GoByteWriter {
+	return &GoByteWriter{w}
+}
+
+func (w *GoByteWriter) Write(p []byte) (n int, err error) {
+	for i, b := range p {
+		if i > 0 && i%30 == 0 {
+			_, err := w.w.Write([]byte("\n"))
+			if err != nil {
+				return i, err
+			}
+		}
+
+		w.w.Write([]byte(fmt.Sprintf("0x%X, ", b)))
+		if err != nil {
+			return i, err
+		}
+	}
+
+	return len(p), nil
+}
